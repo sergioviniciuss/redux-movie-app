4f12cb57ee6e528f4173c9b12ccfe1769e23afb8
sergioviniciuss <sergioviniciuss@gmail.com>
2017-11-05 11:14:59 -0200

    loading movies and updating the store

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import createStore from './store';
+import store from './store';
+import { requestMovies } from './movieApi';
+import { MOVIES_LOADED } from './actions';
+
+requestMovies().then(movies => store.dispatch({ type: MOVIES_LOADED, movies }));
