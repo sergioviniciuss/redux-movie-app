00af3eb7307202ec53fe2576c83a7b7d89965271
sergioviniciuss <sergioviniciuss@gmail.com>
2017-11-05 01:18:24 -0200

    Adding redux to the app, creating actions and reducers

---
+++ src/reducers.js
@@ -0,0 +1,43 @@
+import {
+    FILTER_CHANGED, 
+    MOVIES_LOADED, 
+    FAVORITED, 
+    UNFAVORITED
+} from './actions';
+
+export function movies(state=[], action) {
+    switch (action.type) {
+        case MOVIES_LOADED  :
+            return action.movies;
+        default:
+            return state;
+    }
+}
+
+export function loading(state = true, action) {
+    switch (action.type) {
+        case MOVIES_LOADED:
+            return false;    
+        default:
+            return state;
+    }
+}
+
+export function filter(state = false, action) {
+    switch (action.type) {
+        case FILTER_CHANGED:
+            return action.filter;
+    
+        default:
+            return state;
+    }
+}
+
+export function favorites(state=[], action) {
+    switch (action.type) {
+        case FAVORITED:
+            return [...state, action.movieId]
+        default:
+            return state;
+    }
+}
