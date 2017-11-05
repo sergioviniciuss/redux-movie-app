755f57c5f1e31654bfa6b74b3a1fe707be0173c4
sergioviniciuss <sergioviniciuss@gmail.com>
2017-11-05 15:50:04 -0200

    Rendering the filter

---
+++ src/Filter.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import { FILTER_CHANGED } from './actions';
+import Checkbox from './Checkbox';
+
+
+function mapStateToProps(state) {
+    return {
+        checked: state.filter
+    }
+}
+function mapDispatchToProps(dispatch) {
+    return {
+        onChange: filter => {
+            dispatch({type: FILTER_CHANGED, filter});
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkbox);
